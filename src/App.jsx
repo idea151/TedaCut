6abf9c5843c97f833a6ec868d2ea8e49799d756f
idea151 <iwrayyiroj@gmail.com>
2026-04-06 12:21:43 +0700

    Create src/App.jsx as the main React component importing SubtitleGenerator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SubtitleGenerator from './SubtitleGenerator';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to TedaCut</h1>
+            <SubtitleGenerator />
+        </div>
+    );
+};
+
+export default App;
